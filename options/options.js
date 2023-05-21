1ed072f112d15be884238b97ed28a2f826697ec3
Crowdbotics <engineering@crowdbotics.com>
2023-05-21 13:20:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fancy-leaf-85",
+  url: "https://fancy-leaf-85.botics.co",
+  api: "https://fancy-leaf-85.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
